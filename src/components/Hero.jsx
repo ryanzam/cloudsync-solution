9124e315ecfc4fa25845c6be0996174f3c643508
ranjam <ryanzam2005@gmail.com>
2025-08-24 18:05:15 +0545

    add new hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ScrambledText from './RB/ScrambledText/ScrambledText';
+import Prism from './RB/Prism/Prism';
 
 const Hero = () => {
     return (
 
         <section className="min-w-screen min-h-screen flex flex-col justify-center items-center bg-black">
             <ScrambledText
-                className="pt-20 mb-10 text-center"
+                className="pt-20 mb-10 text-center z-10"
                 radius={100}
                 duration={1.2}
                 speed={0.5}
@@ -16,7 +17,7 @@ const Hero = () => {
             </ScrambledText >
 
             <ScrambledText
-                className="mt-0 text-center"
+                className="mt-0 text-center z-10"
                 radius={100}
                 duration={1.2}
                 speed={0.5}
@@ -25,29 +26,24 @@ const Hero = () => {
                 Boost productivity with our all-in-one SaaS platform designed for modern teams.
             </ScrambledText >
 
-            <button className="bg-white text-blue-600 px-6 py-3 rounded-md font-semibold hover:bg-gray-100">
+            <button className="bg-white text-black px-6 py-3 rounded-md font-semibold hover:bg-gray-100 z-10">
                 Try for Free
             </button>
+
+            <div className='mix-blend-lighten' style={{ width: '100%', height: '600px', position: 'absolute' }}>
+                <Prism
+                    animationType="rotate"
+                    timeScale={0.5}
+                    height={3.5}
+                    baseWidth={5.5}
+                    scale={3.6}
+                    hueShift={0}
+                    colorFrequency={1}
+                    noise={0.5}
+                    glow={1}
+                />
+            </div>
         </section>
-        /*  <section className="min-h-screen flex items-center text-white">
-             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                 <h1 className="text-5xl font-bold mb-4 animate-fade-in">
-                     Streamline Your Workflow with CloudSync
- 
-                 </h1>
-                 
- 
- 
- 
- 
-                 <p className="text-xl mb-6">
-                     Boost productivity with our all-in-one SaaS platform designed for modern teams.
-                 </p>
-                 <button className="bg-white text-blue-600 px-6 py-3 rounded-md font-semibold hover:bg-gray-100">
-                     Try for Free
-                 </button>
-             </div>
-         </section> */
     )
 }
 
